f5187d2449ea2c57937ef85505b8a51c9640771f
xmern <presleyrichnkume@gmail.com>
2024-04-30 18:05:46 -0700

    migrated the models

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import register_user, user_login
 
 urlpatterns = [
-    path('register/',register_user, name='register'),
+    path('register/',register_user),
     path('login/', user_login, name='login')
 
 ]
